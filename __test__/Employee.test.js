0ac4c7f9d7eda8537a588664edaf5545311981e8
Moradi1412 <moradi.javid@gmail.com>
2021-12-05 16:41:02 -0800

    methods getId,getEmail,role

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -16,3 +16,24 @@ test ('will get the emplyee name', () => {
     expect(employee.getName()).toEqual(expect.stringContaining('Robert Chase'));
 });
 
+//testing getting employee id 
+test ('will get the emplyee id', () => { 
+    const employee = new Employee( 'Robert Chase' , '3', 'chase@test.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining('3'));
+});
+
+//testing getting employee email 
+test ('will get the emplyee email', () => { 
+    const employee = new Employee( 'Robert Chase' , '3', 'chase@test.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining('chase@test.com'));
+});
+
+//testing getting employee name 
+test ("will get the emplyee's role", () => { 
+    const employee = new Employee( 'Robert Chase' , '3', 'chase@test.com');
+
+    expect(employee.getRole()).toEqual(expect.stringContaining('Employee'));
+});
+
