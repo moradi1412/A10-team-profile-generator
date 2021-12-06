7913458506d0ed37c78ef3e0fdf70e3db60b55d4
Moradi1412 <moradi.javid@gmail.com>
2021-12-05 16:35:45 -0800

    added method getName

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -2,10 +2,17 @@ const Employee = require ('../lib/Employee');
 
 //testing for new employee 
 test ('create an employee', () => {
-    const employee = new Employee( 'Greg House' , '2', 'house@test.com')
+    const employee = new Employee( 'Greg House' , '2', 'house@test.com');
 
     expect (employee.name).toBe('Greg House');
     expect (employee.id).toBe('2');
     expect (employee.email).toBe('house@test.com');
-})
+}); 
+
+//testing getting employee name 
+test ('will get the emplyee name', () => { 
+    const employee = new Employee( 'Robert Chase' , '3', 'chase@test.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining('Robert Chase'));
+});
 
