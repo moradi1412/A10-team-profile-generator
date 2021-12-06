9360259e07ae346e1716088d3d067e637b761459
Moradi1412 <moradi.javid@gmail.com>
2021-12-05 16:48:53 -0800

    added manger object with test

---
+++ __test__/Manager.test.js
@@ -0,0 +1,18 @@
+const Manager = require('../lib/Manager.js');
+
+//testing for a new manager 
+test('creates a new manager', () => {
+    const manager = new Manager('Lisa Cuddy', '1', 'Cuddy@test.com', '5656');
+    
+    expect(manager.name).toBe('Lisa Cuddy');
+    expect(manager.id).toBe('1');
+    expect(manager.email).toBe('Cuddy@test.com');
+    expect(manager.officeNumber).toBe('5656');
+});
+
+//testing for employee's role 
+test("gets employee's role", () => {
+    const manager = new Manager('Lisa Cuddy', '1', 'Cuddy@test.com', '5656');
+
+    expect(manager.getRole()).toEqual(expect.stringContaining('Manager'));
+});
